fc3cdd5d38b71ec68af2fdcf82ea859306785d06
alvarojuliant <toledojulian113@gmail.com>
2024-11-18 00:14:31 -0300

    subiendo la primer version

---
+++ SistemaMedico/Models/Paciente.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SistemaMedico.Models
+{
+    public class Paciente
+    {
+        public int PacienteId { get; set; } // PK
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public int DNI { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        // Relación
+        public HistoriaClinica HistoriaClinica { get; set; }
+    }
+
+
+}
